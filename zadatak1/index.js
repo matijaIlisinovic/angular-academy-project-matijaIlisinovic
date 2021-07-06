7f1ac9261eae844015b2345ecb6297a2b4ea27c3
machia-nyan <55001997+machia-nyan@users.noreply.github.com>
2021-07-06 14:15:55 +0200

    added form

--- zadatak1/index.js
+++ zadatak1/index.js
@@ -1,1 +1,24 @@
-js 
+let reviews = [
+    {
+        text: "great show, love it",
+        rating: 5
+    },
+    {
+        text: "ok show, I like it",
+        rating: 3
+    },
+    {
+        text: "bad show, hate it",
+        rating: 1
+    }
+]
+
+const rFormElement = document.querySelector("#review-form");
+
+rFormElement.addEventListener("submit", function (event){
+    event.preventDefault();
+
+    const formData = new FormData(rFormElement);
+    const rText = formData.get("review");
+    console.log(rText);
+})
