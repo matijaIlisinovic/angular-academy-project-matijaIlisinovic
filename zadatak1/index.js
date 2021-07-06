e9fe102b4efe03fd3018c425b9d294c349eb4dff
machia-nyan <55001997+machia-nyan@users.noreply.github.com>
2021-07-06 17:26:43 +0200

    add reviews

--- zadatak1/index.js
+++ zadatak1/index.js
@@ -13,12 +13,42 @@ let reviews = [
     }
 ]
 
+const rListElement = document.querySelector("#review-list");
+
 const rFormElement = document.querySelector("#review-form");
+
+function renderReview(review) {
+    const listItem = document.createElement("li");
+    const ratingli = document.createElement("h5");
+  
+    listItem.textContent = review.text;
+    ratingli.textContent = review.rating;
+  
+    listItem.appendChild(ratingli);
+    rListElement.appendChild(listItem);
+}
+
+for (let i = 0; i < reviews.length; i++) {
+    renderReview(reviews[i]);
+}
 
 rFormElement.addEventListener("submit", function (event){
     event.preventDefault();
 
     const formData = new FormData(rFormElement);
+
     const rText = formData.get("review");
-    console.log(rText);
+    const rRating = formData.get("rating");
+
+    const review = {
+        text: rText,
+        rating: rRating
+    }
+    
+
+    reviews.push(review);
+    renderReview(review);
+    
+    formData.set("review", "");
+    formData.set("rating", "");
 })
