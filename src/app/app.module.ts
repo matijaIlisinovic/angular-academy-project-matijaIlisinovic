b68dfe4187075572f5afd2a172541af0b1e2de43
machia-nyan <55001997+machia-nyan@users.noreply.github.com>
2021-07-10 18:56:19 +0200

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatCardModule } from '@angular/material/card';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainLayoutComponent } from './components/main-layout/main-layout.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { AllShowsContainerComponent } from './components/all-shows-container/all-shows-container.component';
+import { ShowListComponent } from './components/show-list/show-list.component';
+import { ShowCardComponent } from './components/show-card/show-card.component';
+import { RatingComponent } from './components/rating/rating.component';
 
 @NgModule({
-	declarations: [AppComponent],
-	imports: [BrowserModule, BrowserAnimationsModule, MatCardModule],
+	declarations: [AppComponent, MainLayoutComponent, SidenavComponent, AllShowsContainerComponent, ShowListComponent, ShowCardComponent, RatingComponent],
+	imports: [BrowserModule, BrowserAnimationsModule, MatCardModule, MatSidenavModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
