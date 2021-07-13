857344e7c7242a75243e0d33ca89a1fcb2a1eef4
machia-nyan <55001997+machia-nyan@users.noreply.github.com>
2021-07-13 21:03:38 +0200

    add links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AllShowsContainerComponent } from './pages/all-shows-container/all-shows-container.component';
 import { MainLayoutComponent } from './components/main-layout/main-layout.component';
+import { TopRatedShowsContainerComponent } from './pages/top-rated-shows-container/top-rated-shows-container.component';
 
 const routes: Routes = [
 	{
@@ -11,6 +12,10 @@ const routes: Routes = [
 			{
 				path: '',
 				component: AllShowsContainerComponent,
+			},
+			{
+				path: 'top-rated',
+				component: TopRatedShowsContainerComponent,
 			},
 		],
 	},
--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,16 +1,27 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
-  selector: 'app-sidenav',
-  templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+	selector: 'app-sidenav',
+	templateUrl: './sidenav.component.html',
+	styleUrls: ['./sidenav.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SidenavComponent implements OnInit {
+	links: Array<any> = [
+		{
+			url: '',
+			title: 'All shows',
+		},
+		{
+			url: '/top-rated',
+			title: 'Top rated',
+		},
+		{
+			url: '/my-profile',
+			title: 'My profile',
+		},
+	];
+	constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
