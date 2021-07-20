c1e74003dec87774aadf941f8f15de68308bd603
machia-nyan <55001997+machia-nyan@users.noreply.github.com>
2021-07-20 16:21:59 +0200

    optimisation

--- src/app/pages/show-details-container/show-details-container.component.ts
+++ src/app/pages/show-details-container/show-details-container.component.ts
@@ -1,6 +1,6 @@
-import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { combineLatest, from, Observable, of } from 'rxjs';
+import { combineLatest, Observable, of } from 'rxjs';
 import { catchError, map, retry, switchMap } from 'rxjs/internal/operators';
 import { Review } from 'src/app/services/review.model';
 import { ReviewsService } from 'src/app/services/reviews.service';
@@ -18,49 +18,35 @@ interface ITemplateData {
 	styleUrls: ['./show-details-container.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ShowDetailsContainerComponent implements OnInit {
+export class ShowDetailsContainerComponent {
 	public isVisible: string = 'block';
 	constructor(private route: ActivatedRoute, private showService: ShowService, private reviewService: ReviewsService) {}
-	public show$: Observable<Show | null> = this.fetchShowFn();
-	public reviews$: Observable<Array<Review>> = this.fetchReviewsFn();
 	public errorDisplay: string = '';
-	public templateData$: Observable<ITemplateData> = combineLatest([this.show$, this.reviews$]).pipe(
-		map(([show, reviews]) => {
-			return {
-				show: show,
-				reviews: reviews,
-			};
-		}),
-		retry(1),
-		catchError((error) => {
-			this.isVisible = 'none';
-			this.errorDisplay = error;
-			return of(error);
+	public templateData$: Observable<ITemplateData> = this.route.paramMap.pipe(
+		switchMap((paramMap) => {
+			const id: string | null = paramMap.get('id');
+			return combineLatest([this.fetchShowFn(id), this.reviewService.getReviews(id)]).pipe(
+				map(([show, reviews]) => {
+					return {
+						show,
+						reviews,
+					};
+				}),
+				retry(1),
+				catchError((error) => {
+					this.isVisible = 'none';
+					this.errorDisplay = error;
+					return of(error);
+				})
+			);
 		})
 	);
 
-	ngOnInit(): void {}
-
-	private fetchShowFn(): Observable<Show | null> {
-		return this.route.paramMap.pipe(
-			switchMap((paramMap) => {
-				const id: string | null = paramMap.get('id');
-				if (id) {
-					return this.showService.getShow(id);
-				}
-				console.log('no id');
-				return of(null);
-			})
-		);
-	}
-	private fetchReviewsFn(): Observable<Array<Review>> {
-		return this.route.paramMap.pipe(
-			switchMap((paramMap) => {
-				const id: string | null = paramMap.get('id');
-				if (id) return this.reviewService.getReviews(id);
-				console.log('no id');
-				return of([]);
-			})
-		);
+	private fetchShowFn(id: string | null): Observable<Show | null> {
+		if (id) {
+			return this.showService.getShow(id);
+		} else {
+			return of(null);
+		}
 	}
 }
