ec95d7282191613f7e60302582090d822ba98a84
machia-nyan <55001997+machia-nyan@users.noreply.github.com>
2021-07-20 09:29:33 +0200

    add id to reviews

--- src/app/services/reviews.service.ts
+++ src/app/services/reviews.service.ts
@@ -11,32 +11,40 @@ export class ReviewsService {
 	private data: Array<IReview> = [
 		{
 			showId: '1',
+			id: '1',
 			comment: 'good show',
 			rating: 4,
 		},
 		{
 			showId: '1',
+			id: '2',
 			comment: 'good show',
 			rating: 3,
 		},
 		{
 			showId: '1',
+			id: '3',
 			comment: 'good show',
 			rating: 5,
 		},
 		{
 			showId: '2',
+			id: '4',
 			comment: 'good show',
 			rating: 3,
 		},
 		{
 			showId: '4',
+			id: '5',
 			comment: 'good show',
 			rating: 3,
 		},
 	];
 
 	private get reviews(): Array<Review> {
+		if (Math.random() < 0) {
+			throw 'b';
+		}
 		return this.data.map((rData: IReview) => {
 			let a = new Review(rData);
 			return a;
@@ -46,15 +54,12 @@ export class ReviewsService {
 		console.log('catching reviews');
 		return of(this.reviews)
 			.pipe(delay(Math.random() * 1000 + 1000))
-			.pipe(map((reviews) => reviews.filter((review: Review) => review.showId === id)));
+			.pipe(
+				map((reviews) => {
+					return reviews.filter((review: Review) => {
+						return review.showId === id;
+					});
+				})
+			);
 	}
-	/*public getShows(): Observable<Array<Show>> {
-		return of(this.shows).pipe(delay(Math.random() * 1000 + 1000));
-	}
-	public getTopRated(): Observable<Array<Show>> {
-		return this.getShows().pipe(map((shows) => shows.filter((show: Show) => show.averageRating > 4)));
-	}
-	public getShow(id: string): Observable<Show | null> {
-		return this.getShows().pipe(map((shows) => shows.find((show: Show) => show.id === id) || null));
-	} */
 }
