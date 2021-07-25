9c2d26704d3e54ea67847e62a329eef81e0bb669
machia-nyan <55001997+machia-nyan@users.noreply.github.com>
2021-07-26 00:52:14 +0200

    fetch real reviews

--- src/app/services/reviews.service.ts
+++ src/app/services/reviews.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { delay, map } from 'rxjs/internal/operators';
@@ -8,57 +9,14 @@ import { Review } from './review.model';
 	providedIn: 'root',
 })
 export class ReviewsService {
-	private data: Array<IReview> = [
-		{
-			showId: '1',
-			id: '1',
-			comment: 'good show',
-			rating: 4,
-		},
-		{
-			showId: '1',
-			id: '2',
-			comment: 'good show',
-			rating: 3,
-		},
-		{
-			showId: '1',
-			id: '3',
-			comment: 'good show',
-			rating: 5,
-		},
-		{
-			showId: '2',
-			id: '4',
-			comment: 'good show',
-			rating: 3,
-		},
-		{
-			showId: '4',
-			id: '5',
-			comment: 'good show',
-			rating: 3,
-		},
-	];
+	constructor(private http: HttpClient) {}
 
-	private get reviews(): Array<Review> {
-		if (Math.random() < 0.1) {
-			throw 'Unable to fetch reviews';
-		}
-		return this.data.map((rData: IReview) => {
-			return new Review(rData);
-		});
-	}
 	public getReviews(id: string | null): Observable<Array<Review>> {
-		console.log('catching reviews');
-		return of(this.reviews)
-			.pipe(delay(Math.random() * 1000 + 1000))
-			.pipe(
-				map((reviews) => {
-					return reviews.filter((review: Review) => {
-						return review.showId === id;
-					});
-				})
-			);
+		return this.http.get<{ reviews: Array<IReview> }>('https://tv-shows.infinum.academy/shows/' + id + '/reviews').pipe(
+			map((response) => {
+				console.log(response);
+				return response.reviews.map((data: IReview) => new Review(data));
+			})
+		);
 	}
 }
