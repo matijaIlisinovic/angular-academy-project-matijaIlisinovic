350f8609978e7ecad7b8ebcd7c092742a3a69312
machia-nyan <55001997+machia-nyan@users.noreply.github.com>
2021-07-26 03:02:24 +0200

    modify rating component (display only)

--- src/app/components/rating/rating.component.ts
+++ src/app/components/rating/rating.component.ts
@@ -12,8 +12,14 @@ export class RatingComponent {
 
 	public starFunction(): string {
 		let starString = '';
+		let j = 5;
 		for (let i = 0; i < this.averageRating - 0.5; i++) {
 			starString = starString.concat(' star');
+			j--;
+		}
+		while (j > 0) {
+			starString = starString.concat(' star_outline');
+			j--;
 		}
 		return starString;
 	}
